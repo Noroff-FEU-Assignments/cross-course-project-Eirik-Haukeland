a428874f403cdb8e9e3d7a6392cc4272e7efd966
Eirik Berget Haukeland <eirik@haukeland.name>
2022-08-17 11:25:01 +0200

    updates to be removed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 import { render_game_card } from "./game_card.js";
-import game from '../furious_game.js'
 
-const btn = document.getElementById('test')
-btn.addEventListener("click", document.getElementById('game_list').append(render_game_card(game)))
+import forge_legend from '../../backend/info_for_games/forge_legend.js'
+import black from '../../backend/info_for_games/black.js.js'
+import furious from '../../backend/info_for_games/furious_game.js'
+import assassin from '../../backend/info_for_games/assassin.js.js'
+
+let games = []
+
+games + forge_legend
+games + black
+games + furious
+games + assassin
+
+console.log(games)
+
+games.forEach(game => {
+  console.log(game)
+  document.getElementById('game_list').append(render_game_card(game))
+});
