95f372d45cb699b355d41984cc49c1ce56f3132d
Eirik Berget Haukeland <eirik@haukeland.name>
2023-01-22 23:49:33 +0100

    ..v.2

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,7 +3,7 @@ import makeGameCard from "./game_card.js";
 
 shoppingCartOnLoad();
 
-fetch(`gamehub.ebh.fyi/wp-json/wc/store/products?featured=true`)
+fetch("gamehub.ebh.fyi/wp-json/wc/store/products?featured=true")
   .then((Response) => Response.json())
   .then((item) => {
     const imbedLocation = document.getElementById("featured-games");
